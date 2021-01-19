e813f1df341c104343524f5dc8f8b6896f795575
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-01-18 23:24:32 -0500

    Set up navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div className="App">
-      Portfolio
-    </div>
+    <Navbar/>
   );
 }
 
