2ac31b151de71790484cee82b741b591450e4019
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-01-19 00:43:28 -0500

    Set up and style navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,54 @@
-import React from 'react'
+import React from "react";
+import logo from "../logo.PNG";
 
 function Navbar() {
   return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark"
 
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-  )
+    >
+      <div className="container-fluid ">
+        <a className="navbar-brand " href="#">
+          <img className=" my-3 py-2 logo" src={logo} alt="logo reading MH" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active " aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link " href="#">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link " href="#">
+                About me
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link " href="#">
+                Contacts
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
